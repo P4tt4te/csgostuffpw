6e8cc57ba0bd2161a34a555d895333d2633c6f20
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-03-05 18:46:08 +0100

    beta 05/03
    -changement de page en javascript
    -overlay inventaire mis à l'échelle pour desktop (1024px de large).

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-/* permet d'afficher ou cacher la div inventaire en appuyant sur le bouton view inventory */
+// permet d'afficher ou cacher la div inventaire en appuyant sur le bouton view inventory //
 
-function masquer() 
-{
-    var divInfo = document.getElementById("inventory");
-    if (divInfo.style.display == 'none')
-        divInfo.style.display = 'flex';
-    else
-        divInfo.style.display = 'none';
+function masquer() {
+  var divInfo = document.getElementById("inventory");
+  if (divInfo.style.display == 'none')
+    divInfo.style.display = 'flex';
+  else
+    divInfo.style.display = 'none';
 }
 
+function changementPage(typeButton){
+  var sectionInfoMain = document.querySelector("#main");
+  var sectionInfoListe = document.querySelector("#liste");
+  if (typeButton == 1) {
+    sectionInfoMain.style.display = 'none';
+    sectionInfoListe.style.display = 'grid';
+  } else {
+    sectionInfoMain.style.display = 'grid';
+    sectionInfoListe.style.display = 'none';
+  }
+}
