7b07b2a928e804b76cc951a40d83959356610811
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-03-08 09:00:17 +0100

    beta 07/03
    -création de la fonction pour gérer la somme total de l'inventaire (uniquement sur le desert eagle)

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// permet d'afficher ou cacher la div inventaire en appuyant sur le bouton view inventory //
+// permet d'afficher ou cacher la div inventaire en appuyant sur le bouton view inventory /
 
 function masquer() {
-  var divInfo = document.getElementById("inventory");
-  if (divInfo.style.display == 'none')
+  var divInfo = document.getElementById('inventory');
+  if (divInfo.style.display == 'none') {
     divInfo.style.display = 'flex';
-  else
+    somme = somme + 10;
+  } else {
     divInfo.style.display = 'none';
+  }
+
 }
 
-function changementPage(typeButton){
-  var sectionInfoMain = document.querySelector("#main");
-  var sectionInfoListe = document.querySelector("#liste");
+function changementPage(typeButton) {
+  var sectionInfoMain = document.querySelector('#main');
+  var sectionInfoListe = document.querySelector('#liste');
   if (typeButton == 1) {
     sectionInfoMain.style.display = 'none';
     sectionInfoListe.style.display = 'grid';
@@ -18,4 +21,13 @@ function changementPage(typeButton){
     sectionInfoMain.style.display = 'grid';
     sectionInfoListe.style.display = 'none';
   }
+}
+
+var sommenbr = 0;
+
+function ajoutSomme(prix) {
+  var somme = document.getElementById('somme');
+  sommenbr = sommenbr + prix;
+  console.log(sommenbr);
+  somme.innerHTML = sommenbr;
 }
