a95814b577ef21b3a62bf55b3d67921b12ba3041
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-07-20 16:29:30 +0200

    20/07 3
    -remplacement du contenu complet par des objets

--- script-vue.js
+++ script-vue.js
@@ -171,7 +171,191 @@ function lancer(evt) {
             type: 'smg',
             source: 'content/guns/weapons/smg/CSGO_UMP-45_Inventory.webp',
             number: 0
-          }
+          },
+          {
+            label: 'AK-47',
+            price: 2700,
+            team: 't',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_AK-47_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'AUG',
+            price: 3300,
+            team: 'ct',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_AUG_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'FAMAS',
+            price: 2050,
+            team: 'ct',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_FAMAS_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'Galil AR',
+            price: 1800,
+            team: 't',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_Galil_AR_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'M4A4',
+            price: 3100,
+            team: 'ct',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_M4A4_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'M4A1-S',
+            price: 2900,
+            team: 'ct',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_M4A1-S_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'SG 553',
+            price: 3000,
+            team: 't',
+            type: 'rifle',
+            source: 'content/guns/weapons/rifles/CSGO_SG_553_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'M249',
+            price: 5200,
+            team: 'all',
+            type: 'lmg',
+            source: 'content/guns/weapons/lmg/CSGO_M249_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'Negev',
+            price: 1700,
+            team: 'all',
+            type: 'lmg',
+            source: 'content/guns/weapons/lmg/CSGO_Negev_Inventory.web',
+            number: 0
+          },
+          {
+            label: 'AWP',
+            price: 4750,
+            team: 'all',
+            type: 'sniper',
+            source: 'content/guns/weapons/sniper/CSGO_AWP_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'G3SG1',
+            price: 5000,
+            team: 't',
+            type: 'sniper',
+            source: 'content/guns/weapons/sniper/CSGO_G3SG1_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'SCAR-20',
+            price: 5000,
+            team: 'ct',
+            type: 'sniper',
+            source: 'content/guns/weapons/sniper/CSGO_SCAR-20_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'SSG 08',
+            price: 1700,
+            team: 'all',
+            type: 'sniper',
+            source: 'content/guns/weapons/sniper/CSGO_SSG_08_Inventory.webp',
+            number: 0
+          },
+          {
+            label: 'HE Grenade',
+            price: 300,
+            team: 'all',
+            type: 'grenade',
+            source: 'content/guns/grenades/Hegrenadehud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Molotov',
+            price: 400,
+            team: 't',
+            type: 'grenade',
+            source: 'content/guns/grenades/Molotovhud.webp',
+            number: 0
+          },
+          {
+            label: 'Incendiary',
+            price: 600,
+            team: 'ct',
+            type: 'grenade',
+            source: 'content/guns/grenades/Incgrenadehud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Flashbang',
+            price: 200,
+            team: 'all',
+            type: 'grenade',
+            source: 'content/guns/grenades/Flashbanghud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Smoke',
+            price: 300,
+            team: 'all',
+            type: 'grenade',
+            source: 'content/guns/grenades/Smokegrenadehud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Decoy',
+            price: 300,
+            team: 'all',
+            type: 'grenade',
+            source: 'content/guns/grenades/Decoyhud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Zeus x27',
+            price: 200,
+            team: 'all',
+            type: 'equipment',
+            source: 'content/guns/equipement/Taserhud.webp',
+            number: 0
+          },
+          {
+            label: 'Kevlar Vest',
+            price: 650,
+            team: 'all',
+            type: 'equipment',
+            source: 'content/guns/equipement/Kevlarhud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Kevlar/Helmet',
+            price: 1000,
+            team: 'all',
+            type: 'equipment',
+            source: 'content/guns/equipement/Assaultsuithud_csgo.webp',
+            number: 0
+          },
+          {
+            label: 'Defuse Kit',
+            price: 400,
+            team: 'ct',
+            type: 'equipment',
+            source: 'content/guns/equipement/Defuserhud_csgo.webp',
+            number: 0
+          },
         ],
         cost: 0,
         list: 0
@@ -190,11 +374,9 @@ function lancer(evt) {
       },
 
       toutSupprimer(){
-        console.log('toutSupprimer');
         this.cost = 0;
         this.list = 0;
         for (let i in this.weapons) {
-          console.log(i);
           this.weapons[i].number = 0;
         }
 
@@ -206,25 +388,6 @@ function lancer(evt) {
   app.mount('#liste');
 
 
-
-  function creationitem(nom,objet) {
-    var newDiv = document.createElement('div');
-    var newButton = document.createElement('button');
-    var delText = document.createTextNode('Del');
-    var newText = document.createTextNode(nom);
-    var inventory = document.querySelector('#inventory_list');
-    newDiv.className = 'guns_list';
-    newButton.className = 'del_guns';
-    newButton.setAttribute('v-on:click','del()');
-    newDiv.appendChild(newText);
-    newDiv.appendChild(newButton);
-    newButton.appendChild(delText);
-    inventory.appendChild(newDiv);
-  }
-
-  function supprimer( evt ) {
-      app.toutSupprimer();
-  }
 
 
 }
